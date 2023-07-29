bacce7decc8198ad57b7a0332db0a1db4bd5c158
seumomo <xo0207@naver.com>
2023-07-30 02:44:05 +0900

    [Feat] 테마 만들기 클릭 시 색상 변경 이벤트 구현

--- client/js/pages/review.js
+++ client/js/pages/review.js
@@ -8,9 +8,13 @@ import {
 
 const showListWrapper = getNode('.showListWrapper');
 const changeArrangeButton = getNode('.changeArrangeButton');
+const body = getNode('body');
+const makeThemeButton = getNode('.makeThemeButton');
+const makeThemeButtonText = getNode('.makeThemeButton__text');
 
 function handleShowMethod(e) {
   const target = e.target.closest('button');
+
   if (!target) {
     return;
   }
@@ -34,23 +38,50 @@ function handleShowMethod(e) {
 function handleChangeArrange(e) {
   const arrangeNew = getNode('.arrangeNew');
   const arrangeViews = getNode('.arrangeViews');
+  const arrowIcon = document.querySelector('.arrowIcon use');
+  const arrangeWrapper = getNode('.arrangeWrapper');
+  const checkClicked = changeArrangeButton.contains(e.target);
+  e.stopPropagation();
 
+  console.log(target);
   changeClass(changeArrangeButton, 'h-[26px]', 'h-[52px]');
-  addClass(arrangeViews, 'hidden');
+  addClass(arrangeNew, 'hidden');
+  changeImageName(arrowIcon, 'up', 'down');
 
   if (changeArrangeButton.classList.contains('h-[52px]')) {
     removeClass(arrangeNew, 'hidden');
     removeClass(arrangeViews, 'hidden');
+    changeImageName(arrowIcon, 'down', 'up');
+    return;
   } else if (changeArrangeButton.classList.contains('h-[26px]')) {
     if (e.target !== arrangeNew && e.target !== arrangeViews) {
       return;
     }
-
     addClass(arrangeNew, 'hidden');
     addClass(arrangeViews, 'hidden');
     removeClass(e.target, 'hidden');
+    return;
   }
 }
 
+function handleMakeTheme(e) {
+  const icon = document.querySelector('.makeThemeButton use');
+  e.preventDefault();
+
+  removeClass(makeThemeButton, '-bg--lion-white');
+  addClass(makeThemeButton, '-bg--lion-primary');
+  addClass(makeThemeButtonText, '-text--lion-white');
+  changeImageName(icon, 'default', 'clicked');
+
+  setTimeout(() => {
+    removeClass(makeThemeButton, '-bg--lion-primary');
+    removeClass(makeThemeButtonText, '-text--lion-white');
+    addClass(makeThemeButton, '-bg--lion-white');
+    addClass(makeThemeButtonText, '-text--lion-gray-300');
+    changeImageName(icon, 'clicked', 'default');
+  }, 200);
+}
+
 showListWrapper.addEventListener('click', handleShowMethod);
-changeArrangeButton.addEventListener('click', handleChangeArrange);
+// body.addEventListener('click', handleChangeArrange);
+makeThemeButton.addEventListener('click', handleMakeTheme);
